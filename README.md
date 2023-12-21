f560b77585b972c5441edc361cf682eab05e45b8
Kari Harju <kari@robocorp.com>
2023-12-21 11:42:36 +0200

    Added image-based
    Updated readme

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# example-windows-calculator
-Simple bot to demostrate Windows desktop capabilities.
+# Simple bot to demostrate Windows desktop automation options.
 
-The robot implements a simple addition on Windows 10 Calculator with different methods:
-* Using the automation id ([see more here](https://robocorp.com/docs/development-guide/desktop/how-to-find-user-interface-elements-using-locators-and-keyboard-shortcuts-in-windows-applications#inspecting-windows-applications-with-accessibility-insights))
-* Using image-based locators
+The robot implements a simple addition on Windows Calculator with different methods:
+1. Using keyboard actions is fast and stable, provided the target application provides hot-keys
+2. Using the locators enables you to target the UI element tree values
+   - See [Robocorp Inspector](https://robocorp.com/docs/developer-tools/visual-studio-code/locators)
+3. Using image-based locators is the slowest and most brittle, but still sometimes needed
+   - Depends a lot on resolution, colors, font
+   - As an example Windows Calculator can look quite different in different cases.
+     ![calculators](.images/calculators.png)
+
+
+The run `log.html` shows the performance differences
+![log example](.images/log.png)
+
--- tasks.py
+++ tasks.py
@@ -1,7 +1,8 @@
 from robocorp.tasks import task
 from robocorp import windows, log
 from robocorp.windows import WindowElement
-import os, time
+from RPA.Desktop import Desktop
+import os
 
 def open_calculator() -> WindowElement:
     windows.desktop().windows_run("calc.exe")
@@ -14,34 +15,40 @@ def clear_calculator(window: WindowElement):
     """
     window.send_keys('{BACK}{BACK}{BACK}{BACK}')
 
-def add_two_numbers_using_ids(window: WindowElement, first: str, second: str ):
-    """
-    Automation using locators based on automation id -field values is a solid way
-    ...if the target app provides static automation ids
-    """
-    window.click('id:clearEntryButton')
-    window.click(f'id:num{first}Button')
-    window.click('id:plusButton')
-    window.click(f'id:num{second}Button')
-    window.click('id:equalButton')
-
-def add_two_numbers_using_ids(window: WindowElement, first: str, second: str ):
-    """
-    Automation using locators based on automation id -field values is a solid way
-    ...if the target app provides static automation ids
-    """
-    window.click('id:clearEntryButton')
-    window.click(f'id:num{first}Button')
-    window.click('id:plusButton')
-    window.click(f'id:num{second}Button')
-    window.click('id:equalButton')
-
-def add_two_numbers_using_send_keys(window: WindowElement, first: str, second: str ):
+def calculate_using_send_keys(window: WindowElement, first: str, second: str ):
     """
     Automation using send_keys() and hotkeys provided by the application is solid and by far the fastest option
     ...if the target app provides keyboard shortcuts
     """
     window.send_keys(f'{first}+{second}=')
+
+def calculate_using_ids(window: WindowElement, first: str, second: str ):
+    """
+    Automation using locators based on automation id -field values is a solid way
+    ...if the target app provides static automation ids
+    """
+    window.click('id:clearEntryButton')
+    window.click(f'id:num{first}Button')
+    window.click('id:plusButton')
+    window.click(f'id:num{second}Button')
+    window.click('id:equalButton')
+
+def calculate_using_image_locators(first: str, second: str):
+    """
+    Automation using image-based locators is the slowest and most brittle form, but sometimes still needed.
+    Image recognition takes time and resources
+    Changes to resolution, themes and UI changes all can break the automation.
+
+    """
+    try:
+        desktop = Desktop()
+        desktop.click(f'alias:{first}')
+        desktop.click('alias:plus')
+        desktop.click(f'alias:{second}')
+        desktop.click('alias:eq')
+    except Exception as e:
+        log.critical('Image-based locator not found')
+        raise e
 
 def log_results(window: WindowElement, screenshot_file: str):
     result = window.get_value('id:CalculatorResults')
@@ -58,20 +65,21 @@ def notepad_automation():
     try:
         window.foreground_window()
 
-        add_two_numbers_using_send_keys(window, 9, 5)
+        calculate_using_send_keys(window, 9, 5)
         log_results(window, 'result-with-send-keys.png')
 
         clear_calculator(window)
-        add_two_numbers_using_ids(window, 9, 5)
+        calculate_using_ids(window, 9, 5)
         log_results(window, 'result-with-ids.png')
 
         clear_calculator(window)
-        add_two_numbers_using_ids(window, 9, 5)
-        log_results(window, 'result-with-ids.png')
+        calculate_using_image_locators(9, 5)
+        log_results(window, 'result-with-images.png')
         
-        # Check out the logs and compare the durations between:
-        # 'add_two_numbers_using_ids' and 'add_two_numbers_using_send_keys'
-
+        # Check out the logs for following points:
+        # - Compare the durations between: 'calculate_using_ids' and 'calculate_using_send_keys'
+        # - Image-based automation will most likely fail:
+        #   The image locators are done for the old style of windows calculator.
 
     finally:
         window.close_window()
