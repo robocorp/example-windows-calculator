dfb565185fbf269d8420ae8d575609246b5febb3
Kari Harju <kari@robocorp.com>
2023-12-21 18:51:39 +0200

    Should work on both calculators
    Fixed .gitignore
    Added .pkgignore to avoid GHA configs from going to package
    Minor GHA workflow fix

--- tasks.py
+++ tasks.py
@@ -9,17 +9,12 @@ def open_calculator() -> WindowElement:
     window = windows.find_window('regex:.*Calculator')
     return window
 
-def clear_calculator(window: WindowElement):
-    """
-    Clear previous calculations by just sending a bunch of backspaces as key events
-    """
-    window.send_keys('{BACK}{BACK}{BACK}{BACK}')
-
 def calculate_using_send_keys(window: WindowElement, first: str, second: str ):
     """
     Automation using send_keys() and hotkeys provided by the application is solid and by far the fastest option
     ...if the target app provides keyboard shortcuts
     """
+    window.send_keys('{BACK}{BACK}{BACK}{BACK}')
     window.send_keys(f'{first}+{second}=')
 
 def calculate_using_ids(window: WindowElement, first: str, second: str ):
@@ -27,13 +22,13 @@ def calculate_using_ids(window: WindowElement, first: str, second: str ):
     Automation using locators based on automation id -field values is a solid way
     ...if the target app provides static automation ids
     """
-    window.click('id:clearEntryButton')
-    window.click(f'id:num{first}Button')
-    window.click('id:plusButton')
-    window.click(f'id:num{second}Button')
-    window.click('id:equalButton')
+    window.click('id:clearEntryButton or name:Clear')
+    window.click(f'id:num{first}Button or name:{first}')
+    window.click('id:plusButton or name:Add')
+    window.click(f'id:num{second}Button or name:{second}')
+    window.click('id:equalButton or name:Equals')
 
-def calculate_using_image_locators(first: str, second: str):
+def calculate_using_image_locators(window: WindowElement, first: str, second: str):
     """
     Automation using image-based locators is the slowest and most brittle form, but sometimes still needed.
     Image recognition takes time and resources
@@ -41,6 +36,7 @@ def calculate_using_image_locators(first: str, second: str):
 
     """
     try:
+        window.click('id:clearEntryButton or name:Clear')
         desktop = Desktop()
         desktop.click(f'alias:{first}')
         desktop.click('alias:plus')
@@ -51,7 +47,7 @@ def calculate_using_image_locators(first: str, second: str):
         raise e
 
 def log_results(window: WindowElement, screenshot_file: str):
-    result = window.get_value('id:CalculatorResults or name:Result')
+    result = window.get_value('id:CalculatorResults or name: "Result"')
     log.info(result)
     result = os.path.join(os.environ.get('ROBOT_ARTIFACTS'), screenshot_file)
     window.screenshot(result)
@@ -68,12 +64,10 @@ def notepad_automation():
         calculate_using_send_keys(window, 9, 5)
         log_results(window, 'result-with-send-keys.png')
 
-        clear_calculator(window)
         calculate_using_ids(window, 9, 5)
         log_results(window, 'result-with-ids.png')
 
-        clear_calculator(window)
-        calculate_using_image_locators(9, 5)
+        calculate_using_image_locators(window, 9, 5)
         log_results(window, 'result-with-images.png')
         
         # Check out the logs for following points:
